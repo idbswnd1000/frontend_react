9b8ed864018575841749e4454ae6c8dc4d59e200
idbswnd1000 <idbswnd1000@gmail.com>
2026-05-13 19:41:54 +0900

    third git

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 const scores = [
-  {이름:"존", 국어: 90, 영어: 90, 수학: 80, 과학:90},
-  {이름:"피터", 국어: 80, 영어: 80, 수학: 90, 과학:80},
-  {이름:"수잔", 국어: 70, 영어: 70, 수학: 80, 과학:70},
-  {이름:"수", 국어: 60, 영어: 60, 수학: 70, 과학:60},]
+  { 이름: "존", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "피터", 국어: 80, 영어: 80, 수학: 90, 과학: 80 },
+  { 이름: "수잔", 국어: 70, 영어: 70, 수학: 80, 과학: 70 },
+  { 이름: "수", 국어: 60, 영어: 60, 수학: 70, 과학: 60 },
+]
 
-const sources=[0,1,2,3]
-tmp=[]
-for (let item of source){
-  tmp.puch(item*2)
-} 
+const sources = [0, 1, 2, 3]
 
-sources.map(item=>(item*2))
+let tmp = []
+
+for (let item of sources) {
+  tmp.push(item * 2)
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,33 +26,20 @@ function App() {
   return (
     <table>
       <tr>
-        <th>이름</th>
-        <th>국어</th>
-        <th>영어</th>
-        <th>수학</th>
-        <th>과학</th>
+        {
+          Object.keys(scores[0]).map(key=>(
+            <th>{key}</th>
+          ))
+        }
       </tr>
-      {[0,1,2,3].map(idx => (
+      {scores.map(item => (
           <tr>
-            <td>{scores[idx].이름}</td>
-            <td>{scores[idx].국어}</td>
-            <td>{scores[idx].영어}</td>
-            <td>{scores[idx].수학}</td>
-            <td>{scores[idx].과학}</td>
+            {Object.values(item).map(
+              value=><td>{value}</td>
+            )}
           </tr>
         )
       )}
-      {/* {[0,1,2,3].map(idx => {
-        return (
-          <tr>
-            <td>{scores[idx].이름}</td>
-            <td>{scores[idx].국어}</td>
-            <td>{scores[idx].영어}</td>
-            <td>{scores[idx].수학}</td>
-            <td>{scores[idx].과학}</td>
-          </tr>
-        )
-      })} */}
     </table>
   )
 }
