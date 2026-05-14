3cdc1a08d8e63a318a27f396f74c37d1b375e07a
idbswnd1000 <idbswnd1000@gmail.com>
2026-05-14 16:02:30 +0900

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import ScoresTable from './components/ScoresTable'
 
-const scores = [
-  { 이름: "존", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
-  { 이름: "피터", 국어: 80, 영어: 80, 수학: 90, 과학: 80 },
-  { 이름: "수잔", 국어: 70, 영어: 70, 수학: 80, 과학: 70 },
-  { 이름: "수", 국어: 60, 영어: 60, 수학: 70, 과학: 60 },
-]
-
-const sources = [0, 1, 2, 3]
-
-let tmp = []
-
-for (let item of sources) {
-  tmp.push(item * 2)
+const style ={
+  width: "500px",
+  height: "300px",
+  display:"flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems:"center",
 }
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleChange = (event) => {
+    console.log(event.target.name)
+  }
   return (
-    <table>
-      <tr>
-        {
-          Object.keys(scores[0]).map(key=>(
-            <th>{key}</th>
-          ))
-        }
-      </tr>
-      {scores.map(item => (
-          <tr>
-            {Object.values(item).map(
-              value=><td>{value}</td>
-            )}
-          </tr>
-        )
-      )}
-    </table>
+    <form>
+      <div>Name: <input
+        type="text"
+        name="Name"
+        value=""
+        onChange={handleChange}/></div>
+      <div>Korean: <input
+        type="number"
+        name="Korean"
+        value=""onChange={handleChange}/></div>
+      <div>English: <input
+      type="number"
+      name="English"
+      value=""onChange={handleChange}/></div>
+      <div>Math: <input
+        type="number"
+        name="Math"
+        value=""onChange={handleChange}/></div>
+      <div>Science: <input
+        type="number"
+        name="Science"
+        value=""onChange={handleChange}/></div>
+      <button>submit</button>
+    </form>
   )
 }
 export default App
---
+++ src/components/ScoresTable.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const scores = [
+  { 이름: "존", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "피터", 국어: 80, 영어: 80, 수학: 90, 과학: 80 },
+  { 이름: "수잔", 국어: 70, 영어: 70, 수학: 80, 과학: 70 },
+  { 이름: "수", 국어: 60, 영어: 60, 수학: 70, 과학: 60 },
+]
+
+function ScoresTable() {
+  return (
+    <table>
+      <tr>
+        {
+          Object.keys(scores[0]).map(key=>(
+            <th>{key}</th>
+          ))
+        }
+      </tr>
+      {scores.map(item => (
+          <tr>
+            {Object.values(item).map(
+              value=><td>{value}</td>
+            )}
+          </tr>
+        )
+      )}
+    </table>
+  )
+}
+export default ScoresTable
